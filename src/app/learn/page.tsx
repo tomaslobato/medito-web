45d42fc0c3cccb315b670ab99acc3c0a34ddeed0
tomaslobato <tlobatodev@gmail.com>
2024-03-29 18:26:20 -0300

    apos

--- src/app/learn/page.tsx
+++ src/app/learn/page.tsx
@@ -4,12 +4,12 @@ export default function Page() {
   return (
     <main className="text-center font-mono w-full flex flex-col items-center h-screen py-14">
       <h1 className="text-3xl">Learn to meditate</h1>
-      <p className="w-3/4 mt-2">With Medito's official meditation begginer course, you can find it on youtube <a href="https://youtube.com/@Medito" className="text-link">here</a></p>
+      <p className="w-3/4 mt-2">With Medito{"'"}s official meditation begginer course, you can find it on youtube <a href="https://youtube.com/@Medito" className="text-link">here</a></p>
       <ul className="flex flex-col p-5 gap-7">
         {audios.learn.map((day) => (
           <li>
             <a
-              href={day.route}
+              href={`/play/${day.id}`}
               className="hover:bg-black hover:text-white text-xl py-3 px-6 border border-black"
             >
               {day.title}
--- src/app/music/page.tsx
+++ src/app/music/page.tsx
@@ -4,8 +4,7 @@ import Link from "next/link";
 export default function Page() {
   return (
     <main className="text-center font-mono w-full flex flex-col items-center h-screen py-14">
-      <h1 className="text-3xl">Learn to meditate</h1>
-      <p className="w-3/4 mt-2">With Medito's official meditation begginer course, you can find it on youtube <a href="https://youtube.com/@Medito" className="text-link">here</a></p>
+      <h1 className="text-3xl">Meditation music</h1>
       <ul className="flex flex-col p-5 gap-7">
         {audios.music.map((day) => (
           <li>
